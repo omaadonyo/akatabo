879678bf9dd825b2017a6284d9d7e1b528a6bad7
Adonyo W. Emmanuel <amazing.chief23@gmail.com>
2026-06-02 20:02:32 +0300

    Logout redirects to landing page / instead of login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,16 @@
 
 namespace App\Providers;
 
+use Filament\Auth\Http\Responses\Contracts\LogoutResponse;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(LogoutResponse::class, \App\Filament\Auth\Responses\LogoutResponse::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
